8a82f370e2b8a412cdd4f08e72d0ac0781fd4c9f
skulos <hendre@mn8.ee>
2022-05-12 18:16:33 +0200

    adding generics

--- bimap.go
+++ bimap.go
@@ -4,20 +4,26 @@ package bimap
 import "sync"
 
 // BiMap is a bi-directional hashmap that is thread safe and supports immutability
-type BiMap struct {
+type BiMap[K comparable, V comparable] struct {
 	s         sync.RWMutex
 	immutable bool
-	forward   map[interface{}]interface{}
-	inverse   map[interface{}]interface{}
+	forward   map[K]V
+	inverse   map[V]K
 }
 
+// Alias for interface{}
+type None struct{}
+
+func (n None) comparable() {}
+
 // NewBiMap returns a an empty, mutable, biMap
-func NewBiMap() *BiMap {
-	return &BiMap{forward: make(map[interface{}]interface{}), inverse: make(map[interface{}]interface{}), immutable: false}
+func NewBiMap[K comparable, V any]() BiMap {
+
+	return BiMap{forward: make(map[K]V), inverse: make(map[K]V), immutable: false}
 }
 
 // Insert puts a key and value into the BiMap, provided its mutable. Also creates the reverse mapping from value to key.
-func (b *BiMap) Insert(k interface{}, v interface{}) {
+func (b *BiMap[K, V]) Insert(k K, v V) {
 	b.s.RLock()
 	if b.immutable {
 		panic("Cannot modify immutable map")
@@ -31,7 +37,7 @@ func (b *BiMap) Insert(k interface{}, v interface{}) {
 }
 
 // Exists checks whether or not a key exists in the BiMap
-func (b *BiMap) Exists(k interface{}) bool {
+func (b *BiMap[K, V]) Exists(k K) bool {
 	b.s.RLock()
 	defer b.s.RUnlock()
 	_, ok := b.forward[k]
@@ -39,7 +45,7 @@ func (b *BiMap) Exists(k interface{}) bool {
 }
 
 // ExistsInverse checks whether or not a value exists in the BiMap
-func (b *BiMap) ExistsInverse(k interface{}) bool {
+func (b *BiMap[K, V]) ExistsInverse(k V) bool {
 	b.s.RLock()
 	defer b.s.RUnlock()
 
@@ -48,18 +54,17 @@ func (b *BiMap) ExistsInverse(k interface{}) bool {
 }
 
 // Get returns the value for a given key in the BiMap and whether or not the element was present.
-func (b *BiMap) Get(k interface{}) (interface{}, bool) {
+func (b *BiMap[K, V]) Get(k K) (V, bool) {
 	if !b.Exists(k) {
-		return "", false
+		return any, false
 	}
 	b.s.RLock()
 	defer b.s.RUnlock()
 	return b.forward[k], true
-
 }
 
 // GetInverse returns the key for a given value in the BiMap and whether or not the element was present.
-func (b *BiMap) GetInverse(v interface{}) (interface{}, bool) {
+func (b *BiMap[K, V]) GetInverse(v V) (K, bool) {
 	if !b.ExistsInverse(v) {
 		return "", false
 	}
@@ -70,7 +75,7 @@ func (b *BiMap) GetInverse(v interface{}) (interface{}, bool) {
 }
 
 // Delete removes a key-value pair from the BiMap for a given key. Returns if the key doesn't exist
-func (b *BiMap) Delete(k interface{}) {
+func (b *BiMap[K, V]) Delete(k interface{}) {
 	b.s.RLock()
 	if b.immutable {
 		panic("Cannot modify immutable map")
@@ -88,7 +93,7 @@ func (b *BiMap) Delete(k interface{}) {
 }
 
 // DeleteInverse emoves a key-value pair from the BiMap for a given value. Returns if the value doesn't exist
-func (b *BiMap) DeleteInverse(v interface{}) {
+func (b *BiMap[K, V]) DeleteInverse(v V) {
 	b.s.RLock()
 	if b.immutable {
 		panic("Cannot modify immutable map")
@@ -108,35 +113,35 @@ func (b *BiMap) DeleteInverse(v interface{}) {
 }
 
 // Size returns the number of elements in the bimap
-func (b *BiMap) Size() int {
+func (b *BiMap[K, V]) Size() int {
 	b.s.RLock()
 	defer b.s.RUnlock()
 	return len(b.forward)
 }
 
 // MakeImmutable freezes the BiMap preventing any further write actions from taking place
-func (b *BiMap) MakeImmutable() {
+func (b *BiMap[K, V]) MakeImmutable() {
 	b.s.Lock()
 	defer b.s.Unlock()
 	b.immutable = true
 }
 
 // GetInverseMap returns a regular go map mapping from the BiMap's values to its keys
-func (b *BiMap) GetInverseMap() map[interface{}]interface{} {
+func (b *BiMap[K, V]) GetInverseMap() map[V]K {
 	return b.inverse
 }
 
 // GetForwardMap returns a regular go map mapping from the BiMap's keys to its values
-func (b *BiMap) GetForwardMap() map[interface{}]interface{} {
+func (b *BiMap[K, V]) GetForwardMap() map[K]V {
 	return b.forward
 }
 
 // Lock manually locks the BiMap's mutex
-func (b *BiMap) Lock() {
+func (b *BiMap[K, V]) Lock() {
 	b.s.Lock()
 }
 
 // Unlock manually unlocks the BiMap's mutex
-func (b *BiMap) Unlock() {
+func (b *BiMap[K, V]) Unlock() {
 	b.s.Unlock()
 }
